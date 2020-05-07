c42d40cd8e3910e15d03c93c788fb7a0e6fdf31f
erraX <niminjiecide@outlook.com>
2020-05-07 23:17:08 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,32 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <el-menu :router="true" mode="horizontal">
+      <el-menu-item index="/project">项目</el-menu-item>
+      <el-menu-item index="/workbench">工作台</el-menu-item>
+      <el-menu-item index="/data">数据</el-menu-item>
+      <el-menu-item index="/docs">文档</el-menu-item>
+    </el-menu>
+    <router-view />
   </div>
 </template>
+
+<script>
+  import {
+    Menu,
+    MenuItem
+  } from 'element-ui';
+  export default {
+    components: {
+      'el-menu': Menu,
+      'el-menu-item': MenuItem
+    }
+  }
+</script>
 
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
